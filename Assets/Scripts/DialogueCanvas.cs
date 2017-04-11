294cde84d6d5eca56e267f48043248eed609edf2
sannaluo <sanna.luostarinen@metropolia.fi>
2017-04-11 15:26:30 +0300

    dialogue started

---
+++ Assets/Scripts/DialogueCanvas.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueCanvas : MonoBehaviour {
+    public GameObject dialogueCanvas;
+
+    void Start() {
+        
+        
+         dialogueCanvasActive();
+    }
+
+    public void dialogueCanvasActive()
+    {
+        dialogueCanvas.SetActive(true);
+    }
+}
+
+
---
+++ Assets/Scripts/ModalPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ModalPanel : MonoBehaviour {
+
+    public Text npc;
+    public Button yesButton;
+    public Button noButton;
+    public Button cancelButton;
+    public GameObject modalPanelObject;
+
+    private static ModalPanel modalPanel;
+
+    public static ModalPanel Instance()
+    {
+        if (!modalPanel)
+        {
+            modalPanel = FindObjectOfType(typeof(ModalPanel)) as ModalPanel;
+            if (!modalPanel)
+            {
+                Debug.LogError("There needs to be one active ModalPanel script on a GameObject in your Scene");
+            }
+            }
+            return modalPanel;
+        
+    }
+
+    public void Choice(string npc, UnityAction yesEvent, UnityAction noEvent, UnityAction cancelEvent)
+    {
+        yesButton.onClick.RemoveAllListeners();
+    }
+
+}
